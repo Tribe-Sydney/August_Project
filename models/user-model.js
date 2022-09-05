5ff09d136c0f9bb7e9d978fe02bf979637eeed2f
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2022-09-05 21:56:54 +0100

    sub

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: [true, "Student must have a name"],
-        trim: true,
-      },
-      phoneNumber: {
-        type: String,
-        required: [true, "Student must have a phone-number"],
-      },
-})
+  fullName: {
+    type: String,
+    required: [true, "Student must have a name"],
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, "Student must have a phone-number"],
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an email"],
+  },
+});
