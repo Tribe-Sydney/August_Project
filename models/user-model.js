15e7ac72b7b7c9ff67dfccb40a58e15cbaa9a151
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2022-09-05 23:28:32 +0100

    setting up

--- models/user-model.js
+++ models/user-model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
 const userSchema = new mongoose.Schema({
   fullName: {
     type: String,
@@ -12,5 +14,19 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "A user must have an email"],
+    validate: [validator.isEmail, "Please enter a valid email"],
+    trim: true,
+    lowerCase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "A user must have an email"],
+    minLength: [8, "A password must be at least 8 characters"],
+    select: false,
   },
 });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
