757bc803007d5eb33ae4ab1e8bab6f0e9600ace5
p-agboola <peterolamilekan65@gmail.com>
2022-09-05 22:10:25 +0100

    femi

--- models/user-model.js
+++ models/user-model.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+      },
+      userName: {
+        type: String,
+        required: [true, "username is required"],
+        trim: true,
+      },
+      password: {
+        type: String,
+        required: [true, "password is required"],
+        trim: true,
+      },
     fullName: {
         type: String,
-        required: [true, "Student must have a name"],
+        required: [true, "fullname is required"],
         trim: true,
       },
       phoneNumber: {
         type: String,
-        required: [true, "Student must have a phone-number"],
+        required: [true, "phone number is required"],
       },
 })
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
--- user.js
+++ user.js
@@ -3,7 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config({path:'./config,env'})
 const mongoose =require('mongoose')
-
+const User = require('./model/user-model')
 const user = express().
 
 //middleware
@@ -19,7 +19,7 @@ mongoose.connect(process.env.DB_URL)
 
 
     //posting user signup to the database
-    app.post('/signup', async(req, res) =>
+    user.post('/signup', async(req, res) =>
 {
     const reqBody = req.body;
     console.log(reqBody, req.body);
@@ -30,20 +30,30 @@ mongoose.connect(process.env.DB_URL)
 })
 
 //posting user login to the database
-app.post("/login", async(req,res) =>
+user.post("/login", async(req,res) =>
 {
 const {email, password} = req.body;
-const user = await user.findOne ({email, password});
-if (!user) 
-return res.status (404).send("user not found");
-return res.status (200).send(user);
+const user = await User.findOne ({email, password});
+if (!user) {
+    return res.status (404).send("user not found")
+    } 
+    return res.status (200).send(user);
 })
 
 
-
-
-
-
+// Delete Account
+user.delete('/id', async (req, res)=>{
+    try {
+      await User.findByIdAndDelete(req.params.id);res.status(200).json({
+        status:'successful deleted',
+       })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+})
 
 
 
