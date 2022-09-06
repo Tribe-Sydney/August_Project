a2f023a892885ae1e5a147ecc55fe07eaa462e2f
p-agboola <peterolamilekan65@gmail.com>
2022-09-06 02:38:00 +0100

    updated

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -53,6 +53,13 @@ exports.signIn = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
   try {
+    const user = await User.findById(req.params.id);
+    if(!user){
+      return res.status(400).json({
+        status: 'fail',
+        message: `There is no user with the id ${req.params.id}`
+      })
+    }
     await User.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: "successful deleted",
@@ -102,34 +109,3 @@ exports.updateUser = async (req, res) => {
     });
   }
 };
-
-//Get All Users
-exports.getAll = async (req, res) => {
-  try{
-      const user = await User.find()
-      res.status(200).json({
-          results: user.length,
-          data: user
-      })
-  } catch (err) {
-      res.status(400).json({
-          status: 'fail',
-          message: err
-      })
-  }
-}
-
-//Get One User
-exports.getOne =  async (req, res) => {
-  try{
-      const user = await User.findById(req.params.id)
-      res.status(200).json({
-          data: user
-      })
-  } catch (err) {
-      res.status(400).json({
-          status: 'fail',
-          message: err
-      })
-  }
-}
