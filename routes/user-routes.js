954a73ee3856bcadf6ff25f61555c4195b2fafbb
Joshua Olutunde <joshuafavour81@gmail.com>
2022-09-06 02:23:35 +0100

    added get request

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,11 +4,14 @@ const {
   signIn,
   deleteUser,
   updateUser,
+  getAll,
+  getOne
 } = require("../controllers/user-controllers");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
-router.route("/:id").delete(deleteUser).patch(updateUser);
+router.get("/signin", getAll);
+router.route("/:id").delete(deleteUser).patch(updateUser).get(getOne);
 module.exports = router;
